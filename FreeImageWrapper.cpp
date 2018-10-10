d9f63d113f6fa3acf37563927ef68b1da0e8c283
ithewei <ithewei@163.com>
2018-10-10 12:21:57 +0800

    add FreeImageWrapper

---
+++ FreeImageWrapper.cpp
@@ -0,0 +1,96 @@
+#include "FreeImageWrapper.h"
+
+bool FreeImageWrapper::s_bInit = false;
+
+FreeImageWrapper::FreeImageWrapper(){
+    Init();
+    
+    _bmp = NULL;
+    _mem = NULL;
+}
+
+FreeImageWrapper::~FreeImageWrapper(){
+    unload();
+}
+
+void FreeImageWrapper::Init(){
+    if (s_bInit == false){
+        s_bInit = true;
+        FreeImage_Initialise();
+    }
+}
+
+void FreeImageWrapper::DeInit(){
+    if (s_bInit){
+        s_bInit = false;
+        FreeImage_DeInitialise();
+    }
+}
+
+void FreeImageWrapper::unload(){
+    if (_bmp){
+        FreeImage_Unload(_bmp);
+        _bmp = NULL;
+    }
+
+    if (_mem){
+        FreeImage_CloseMemory(_mem);
+        _mem = NULL;
+    }
+}
+
+bool FreeImageWrapper::LoadFromFile(const char* filepath){
+    unload();
+
+    FREE_IMAGE_FORMAT fmt = FreeImage_GetFileType(filepath);
+    if (fmt == FIF_UNKNOWN){
+        return false;
+    }
+
+    _bmp = FreeImage_Load(fmt, filepath);
+    return _bmp != NULL;
+}
+
+bool FreeImageWrapper::LoadFromMemory(const void* data, size_t size){
+    unload();
+
+    _mem = FreeImage_OpenMemory((BYTE*)data, size);
+    if (_mem == NULL){
+        return false;
+    }
+
+    FREE_IMAGE_FORMAT fmt = FreeImage_GetFileTypeFromMemory(_mem);
+    if (fmt == FIF_UNKNOWN){
+        return false;
+    }
+
+    _bmp = FreeImage_LoadFromMemory(fmt, _mem);
+    return _bmp != NULL;
+}
+
+#include "hw/hlog.h"
+#include "hw/hstring.h"
+#include "hw/hplatform.h" // for stricmp in linux
+#include <string.h>
+bool FreeImageWrapper::Save(const char* filepath){
+    if (_bmp == NULL){
+        return false;
+    }
+
+    // for the moment, we just provide bmp/jpg/png
+    string strFile(filepath);
+    const char* suffix = suffixname(strFile).c_str();
+    FREE_IMAGE_FORMAT fmt = FIF_UNKNOWN;
+    if (stricmp(suffix, "bmp") == 0){
+        fmt = FIF_BMP;
+    }else if (stricmp(suffix, "jpg") == 0){
+        fmt = FIF_JPEG;
+    }else if (stricmp(suffix, "png") == 0){
+        fmt = FIF_PNG;
+    }else{
+        hloge("Unsupported format %s", suffix);
+        return false;
+    }
+
+    return FreeImage_Save(fmt, _bmp, filepath);
+}
---
+++ FreeImageWrapper.h
@@ -0,0 +1,49 @@
+#ifndef FREE_IMAGE_WRAPPER_H
+#define FREE_IMAGE_WRAPPER_H
+
+#include "FreeImage.h"
+
+class FreeImageWrapper{
+public:
+    FreeImageWrapper();
+    ~FreeImageWrapper();
+
+    static void Init();
+    static void DeInit();
+
+    bool LoadFromFile(const char* filepath);
+    bool LoadFromMemory(const void* data, size_t size);
+
+    bool Save(const char* filepath);
+
+    int GetWidth() {
+        return FreeImage_GetWidth(_bmp);
+    }
+    int GetHeight() {
+        return FreeImage_GetHeight(_bmp);
+    }
+    int GetPitch() {
+        return FreeImage_GetPitch(_bmp);
+    }
+    int GetBPP() {
+        return FreeImage_GetBPP(_bmp);
+    }
+    BYTE* GetData() {
+        return FreeImage_GetBits(_bmp);
+    }
+    size_t GetSize() {
+        return GetPitch()*GetHeight();
+    }
+
+protected:
+    void unload();
+
+public:
+    FIBITMAP* _bmp;
+    FIMEMORY* _mem;
+
+private:
+    static bool s_bInit;
+};
+
+#endif // FREE_IMAGE_WRAPPER_H

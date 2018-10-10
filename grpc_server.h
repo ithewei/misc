93e7ed96221b35d6a9ae1152fc2af64641007b2b
ithewei <ithewei@163.com>
2018-10-10 12:21:29 +0800

    add GrpcServer

---
+++ grpc_server.h
@@ -0,0 +1,33 @@
+#ifndef GRPC_SERVER_H
+#define GRPC_SERVER_H
+
+#include "grpc++/grpc++.h"
+using grpc::ServerBuilder;
+using grpc::Server;
+using grpc::Service;
+
+#include "hw/hstring.h"
+
+class GrpcServer {
+public:
+    GrpcServer(int port) : _port(port) {
+        string srvaddr = asprintf("%s:%d", "0.0.0.0", _port);
+        build_.AddListeningPort(srvaddr, grpc::InsecureServerCredentials());
+    }
+
+    void RegisterService(Service* service) {
+        build_.RegisterService(service);
+    }
+
+    void Run() {
+        auto server = build_.BuildAndStart();
+        server->Wait();
+    }
+
+public:
+    int _port;
+private:
+    ServerBuilder build_;
+};
+
+#endif // GRPC_SERVER_H
